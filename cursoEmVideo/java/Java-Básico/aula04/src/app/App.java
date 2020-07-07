4ca843b1afe119c5905684b9e50f559253d5def3
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-07-07 19:24:27 -0300

    aula04 e seus desafios prontos

--- cursoEmVideo/java/Java-Básico/aula04/src/app/App.java
+++ cursoEmVideo/java/Java-Básico/aula04/src/app/App.java
@@ -1,7 +1,18 @@
 package app;
 
+import java.awt.*;
+import java.util.*;
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        Date data = new Date();
+        Locale idioma = Locale.getDefault();
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        System.out.println("Data: " + data.toString());
+        System.out.println("Lingua Sistema: " + idioma.getDisplayLanguage());
+        System.out.println("Reolução da Tela: " + d.width + "x" + d.height );
     }
 }
