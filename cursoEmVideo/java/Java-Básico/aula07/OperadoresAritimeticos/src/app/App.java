953f77d0a7a6f2d497087262efba55e428feb78e
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-07-14 16:03:38 -0300

    aula07-teo

---
+++ cursoEmVideo/java/Java-Básico/aula07/OperadoresAritimeticos/src/app/App.java
@@ -0,0 +1,40 @@
+package app;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 =3;
+		int n2 = 5;
+		float m = (n1+n2)/2;
+		System.out.printf("A média de %d e %d é %.1f\n",n1,n2, m);
+		
+		int numero = 5;
+		int valor = 5 + numero++;
+		System.out.printf("Pós : %s\n", valor);
+		numero=5;
+		valor = 5 + ++numero;
+		System.out.printf("Pré : %s\n", valor);
+
+		
+		numero = 4;
+		numero *=2;
+		System.out.print(numero+ "\n");
+		
+		float v = 8.5f;
+		int ar = (int) Math.floor(v);
+		System.out.printf("O arredondamendo floor de %.1f é %d\n",v,ar);
+		
+		ar = (int) Math.round(v);
+		System.out.printf("O arredondamendo round de %.1f é %d\n",v,ar);
+		
+		double ale = Math.random();
+		int sup=11; //nao inclui o superior
+		int sub=5;
+		ale = sub + ale*((sup--)-sub);
+		System.out.printf("Valor aleatorio entre %d e %d é %d\n",sub,sup,(int)ale);
+		
+		System.out.println();	
+	}
+
+}
---
+++ cursoEmVideo/java/Java-Básico/aula07/OperadoresSwing/src/classes/TelaValores.java
@@ -0,0 +1,130 @@
+package classes;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.Box;
+import java.awt.Canvas;
+import java.awt.Component;
+import java.awt.Dimension;
+import javax.swing.JSlider;
+import javax.swing.JSeparator;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+
+public class TelaValores extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtNum;
+	private JTextField txtDen;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaValores frame = new TelaValores();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaValores() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 295, 375);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtNum = new JTextField();
+		txtNum.setBounds(150, 54, 114, 19);
+		contentPane.add(txtNum);
+		txtNum.setColumns(10);
+		
+		txtDen = new JTextField();
+		txtDen.setBounds(150, 97, 114, 19);
+		contentPane.add(txtDen);
+		txtDen.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Numerador");
+		lblNewLabel.setBounds(38, 56, 79, 15);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Denominador");
+		lblNewLabel_1.setBounds(38, 99, 95, 15);
+		contentPane.add(lblNewLabel_1);
+		
+		JButton btnDividir = new JButton("Calcular");
+		
+		btnDividir.setBounds(72, 144, 117, 25);
+		contentPane.add(btnDividir);
+		
+		JLabel lblNewLabel_2 = new JLabel("Divisao");
+		lblNewLabel_2.setBounds(38, 218, 70, 15);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblDiv = new JLabel("0");
+		lblDiv.setForeground(Color.RED);
+		lblDiv.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblDiv.setBounds(171, 218, 70, 15);
+		contentPane.add(lblDiv);
+		
+		JLabel lblNewLabel_4 = new JLabel("Resto");
+		lblNewLabel_4.setBounds(38, 260, 70, 15);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblRest = new JLabel("0");
+		lblRest.setForeground(Color.RED);
+		lblRest.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblRest.setBounds(171, 260, 70, 15);
+		contentPane.add(lblRest);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("Raiz");
+		lblNewLabel_4_1.setBounds(38, 301, 70, 15);
+		contentPane.add(lblNewLabel_4_1);
+		
+		JLabel lblRaiz = new JLabel("0");
+		lblRaiz.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblRaiz.setForeground(new Color(255, 0, 0));
+		lblRaiz.setBounds(171, 301, 70, 15);
+		contentPane.add(lblRaiz);
+		
+		btnDividir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int num = Integer.parseInt(txtNum.getText());
+				int dev = Integer.parseInt(txtDen.getText());
+				String div = Integer.toString(num/dev);
+				String resto = Integer.toString(num%dev);
+				lblDiv.setText(div);
+				lblRest.setText(resto);
+				String raiz= Double.toString(Math.pow((double)num,1.0D/((double)dev)));
+				lblRaiz.setText(raiz);
+				
+				
+			}
+		});
+	}
+}
