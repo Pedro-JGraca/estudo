c1851a833149e0d945baeacc54671c696792c6a5
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-07-21 02:08:03 -0300

    new file:   execute-bin.sh 	new file:   execute-compilando.sh

---
+++ cursoEmVideo/java/Java-Básico/aula08/operadoresLogicos/execute-bin.sh
@@ -0,0 +1,3 @@
+cd bin/
+java operadoresLogicos.TelaIdade
+cd ..
---
+++ cursoEmVideo/java/Java-Básico/aula08/operadoresLogicos/execute-compilando.sh
@@ -0,0 +1,5 @@
+javac -d compilado/ src/operadoresLogicos/*.java
+cd compilado/
+java operadoresLogicos.TelaIdade
+cd ..
+rm -rf compilado/
