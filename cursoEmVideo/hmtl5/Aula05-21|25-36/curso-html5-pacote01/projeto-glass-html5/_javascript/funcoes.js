c0cf65caf10d6c8804b43e97bc504c1af8f5d80b
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-08-03 02:45:11 -0300

    aula 32 completa!

--- cursoEmVideo/hmtl5/Aula05-21|25-36/curso-html5-pacote01/projeto-glass-html5/_javascript/funcoes.js
+++ cursoEmVideo/hmtl5/Aula05-21|25-36/curso-html5-pacote01/projeto-glass-html5/_javascript/funcoes.js
@@ -1,3 +1,10 @@
 function mudaFoto (id,foto) {
     document.getElementById(id).src =  foto;
 }
+
+function latetal(){
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+    return width;
+}
+
