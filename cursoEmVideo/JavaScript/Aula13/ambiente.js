cab6791864ba99b1b28ae293b55c9b0e3704f702
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-08-16 13:00:46 -0300

    aula 14 feita

--- cursoEmVideo/JavaScript/Aula13/ambiente.js
+++ cursoEmVideo/JavaScript/Aula13/ambiente.js
@@ -1,14 +1,11 @@
 
-var inicio = 1;
-var c =inicio;
-var meio = 500;
-var fim = 1;
+var inicio = 1, c =inicio, meio = 3, fim = 1;
 
 while (c<meio) {
-    console.log(`passo ${c}`);
+    console.log("passo " + c);
     c++;
 }//testa e faz
 do {//faz e testa
-    console.log(`passo ${c}`);
+    console.log("passo " + c);
     c--;
 }while(c>=fim);
---
+++ cursoEmVideo/JavaScript/Aula14/ambiente.js
@@ -0,0 +1,10 @@
+var inicio =1, meio = 3, fim = 1;
+
+console.log("começando");
+for (var c = inicio; c<meio; c++) {
+    console.log("passo " + c);
+}
+for (; c>=fim; c--) {
+    console.log("passo " + c);
+}
+console.log("terminando");
