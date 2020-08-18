c7dc501f556d1c6bcb2f84e72072a693f7312e6c
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-08-18 17:27:25 -0300

    aula 16

---
+++ cursoEmVideo/JavaScript/Aula16/ambiente.js
@@ -0,0 +1,22 @@
+function parImpar(n=0) {
+    if(n%2 == 0) {
+        return "par";
+    }
+    else {
+        return "impar";
+    }
+}
+
+function soma (n1=0,n2=0) {
+    return n1 + sn2;
+}
+
+console.log(parImpar(5));
+
+console.log(soma(1,2));
+
+let v = function (x) {
+    return x*2;
+};
+console.log(v(5));
+
