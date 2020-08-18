9b03cf1bc4388e307ba4aa8df1ec768ee716851b
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-08-18 10:53:57 -0300

    Aula 14- ex1- super Contador

---
+++ cursoEmVideo/JavaScript/Aula14-ex/super-contador/script.js
@@ -0,0 +1,74 @@
+
+var inicio =window.document.getElementById("inicio"), fim = window.document.getElementById("fim"), passo = window.document.getElementById("passo");
+
+var res = window.document.getElementById('res');
+
+function disparar () {
+    if (!inicio.value) {//verifica se inicio possui algum valor
+        res.style.textAlign = "center";
+        res.innerText = "Inicio Precisa de valor!";
+    }
+
+    else if (!fim.value) {//verifica se fim possui algum valor
+        res.style.textAlign = "center";
+        res.innerText = "Fim Precisa de valor!";
+    }
+
+    else if (!passo.value) {//verifica se passo possui algum valor
+        res.style.textAlign = "center";
+        res.innerText = "Passo Precisa de valor!";
+    }
+
+    else {//entra se todos tem valores
+        let Inicio= Number(inicio.value);
+        let Fim = Number(fim.value);
+        let Passo = Number(passo.value);
+            
+        //verifica se os valores de Inicio, Fim e Passo são válidos
+        if (Passo==0) {//verifica se passo é igual a zero
+            res.style.textAlign = "center";
+            res.innerText ="Passo precisa ser diverente de zero";
+        }
+        else if (Fim==Inicio) { //verifica se Inicio e Fim são Iguais
+            res.style.textAlign = "center";
+            res.innerText ="Inicio e Fim precisam ser distintos";
+        }
+        else if ((Inicio<Fim)&&(Passo<0)) { //Se Inicio < Fim e Passo < 0
+            res.style.textAlign = "center";
+            res.innerText ="Nesse Caso Passo precisa ser um valor positivo";
+        }
+        else if ((Inicio>Fim)&&(Passo>0)) { //se Inicio > Fim e Passo >0
+            res.style.textAlign = "center";
+            res.innerText ="Nesse Caso Passo precisa ser um valor negativo";
+        }
+
+        else {//se todos os valores testados são validos
+            res.innerText ="";
+        
+            res.style.textAlign = "justify";
+            var c = Inicio;
+            if (Inicio<Fim) { //for crescente
+                for (;c<=Fim;c+=Passo) {
+                    res.innerText += " " + c.toString();
+                    res.innerText += " ->";
+                }
+                res.innerText += " fim";
+            }
+            else if (Inicio>Fim) {//for descrescente
+                for (;c>=Fim;c+=Passo) {
+                    res.innerText += " " + c.toString();
+                    res.innerText += " ->";
+                }
+                res.innerText += " fim";
+            }
+            else {
+                res.style.textAlign = "center";
+                res.innerText =" Caso não Previsto!";
+            }
+
+
+        }
+    }
+        
+}
+
