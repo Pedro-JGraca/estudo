d7ed3b33ac0a8ffc56b4604c453a17d2f8c75715
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2021-05-30 04:43:47 -0300

    acabei o vídeo

--- React/cursosRapidos/LucasNhimi/clone_youtube/src/App.js
+++ React/cursosRapidos/LucasNhimi/clone_youtube/src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { Button, createMuiTheme, ThemeProvider } from "@material-ui/core";
+import React, { useState } from 'react';
+import {createMuiTheme, ThemeProvider } from "@material-ui/core";
 
 import Home from './home'
+import { dark } from '@material-ui/core/styles/createPalette';
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
   const theme = createMuiTheme({
+    spacing: 4,
     palette: {
+      type: darkMode ? 'dark': 'light', 
+
       primary: {
         main: '#f44336',
       },
       secondary: {
-        main: '#3f51b5',
+        main: '#3EA6FF',
+      },
+      background:{
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF',
       }
     }
 
@@ -18,7 +30,7 @@ function App() {
 
   return <div>
     <ThemeProvider theme={theme}>
-      <Home />
+      <Home darkMode={darkMode} setDarkMode={setDarkMode} />
     </ThemeProvider>
   </div>;
 }
