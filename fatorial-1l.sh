dee6076305ea59dc84e260460c86fe0fa7212103
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2021-10-02 19:50:37 -0300

    detalhe

--- fatorial-1l.sh
+++ fatorial-1l.sh
@@ -1,1 +1,1 @@
-S=1 && for (( A=$1; A>=0; A--)); do [[ $A -eq 0  ]] && echo $S || [[ $A -eq $1 ]] && S=$A || S=$[$S*($A)] ;done 
+S=1 && for (( A = $1; A>=0; A--)); do [[ $A -eq 0  ]] && echo $S || [[ $A -eq $1 ]] && S=$A || S=$[$S*($A)] ;done 
